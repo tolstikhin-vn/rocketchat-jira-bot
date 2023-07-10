3d511ecb18981a4e7387809736fa24ac837aa53b
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-07-10 17:57:27 +0300

    В БД добавляется дата и время создания задачи

--- src/app/models.py
+++ src/app/models.py
@@ -1,6 +1,14 @@
 import logging
 import json
-from sqlalchemy import create_engine, Column, Integer, String
+from datetime import datetime
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+)
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
@@ -77,7 +85,11 @@ def insert_task_record(id_user, task_link):
         user = session.query(User).filter(User.user_id == id_user).first()
         if user:
             # Вставка записи в tasks_log
-            new_task_log = TaskLog(user=user.id, task_link=task_link)
+            new_task_log = TaskLog(
+                user=user.id,
+                task_link=task_link,
+                datetime_creating=datetime.now(),
+            )
             session.add(new_task_log)
             session.commit()
     except SQLAlchemyError as ex:
@@ -95,6 +107,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_name = Column(String(50), nullable=False)
     user_id = Column(String(20), nullable=False, unique=True)
+    is_admin = Column(Boolean, nullable=False, default=False)
 
 
 class TaskLog(Base):
@@ -103,3 +116,4 @@ class TaskLog(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user = Column(Integer, nullable=False)
     task_link = Column(String(100), nullable=False)
+    datetime_creating = Column(DateTime, nullable=False)

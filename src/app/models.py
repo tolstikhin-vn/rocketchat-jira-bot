620a072ae06cd17bde338498a6293125287a78d0
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2023-07-10 11:51:55 +0300

    Данные для подключения вынесены Добавлена обработка исключений

--- src/app/models.py
+++ src/app/models.py
@@ -1,29 +1,55 @@
 import logging
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+import json
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
 
-url_object = URL.create(
-    drivername='mysql+pymysql',
-    username='mysqluser',
-    password='',
-    host='',
-    port='3306',
-    database='rocketchat_db',
-)
-
-engine = create_engine(url_object, echo=False)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
 Base = declarative_base()
+
+
+def create_session():
+    """Создать сессию для работы с базой данных"""
+    try:
+        with open('src/data/config_mysql.json') as db_config_file:
+            config_data = json.load(db_config_file)
+
+        drivername = config_data['drivername']
+        username = config_data['username']
+        password = config_data['password']
+        host = config_data['host']
+        port = config_data['port']
+        database = config_data['database']
+
+        url_object = URL.create(
+            drivername, username, password, host, port, database
+        )
+        engine = create_engine(url_object, echo=False)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        return session
+    except (FileNotFoundError, KeyError, SQLAlchemyError) as ex:
+        logging.error(f'Ошибка при создании сессии базы данных: {ex}')
+        return None, None
+
+
+def close_session(session):
+    """Закрыть сессию базы данных"""
+    try:
+        session.close()
+    except SQLAlchemyError as ex:
+        logging.error(f'Ошибка при закрытии сессии базы данных: {ex}')
 
 
 def insert_new_user(user_name, user_id):
     """Добавить в БД информацию о пользователе, который начал диалог с ботом"""
+    session = create_session()
+    if session is None:
+        return
+
     try:
         user = session.query(User).filter_by(user_id=user_id).first()
 
@@ -38,11 +64,14 @@ def insert_new_user(user_name, user_id):
             f'Возникла ошибка при выполнении операции с базой данных: {ex}'
         )
     finally:
-        session.close()
+        close_session(session)
 
 
 def insert_task_record(id_user, task_link):
     """Добавить новую запись в таблицу tasks_log о создании задачи пользователем"""
+    session = create_session()
+    if session is None:
+        return
     try:
         # Получение users.id по user_id
         user = session.query(User).filter(User.user_id == id_user).first()
@@ -57,7 +86,7 @@ def insert_task_record(id_user, task_link):
             f'Возникла ошибка при выполнении операции с базой данных: {ex}'
         )
     finally:
-        session.close()
+        close_session(session)
 
 
 class User(Base):
@@ -66,7 +95,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_name = Column(String(50), nullable=False)
     user_id = Column(String(20), nullable=False, unique=True)
-    # tasks_log = relationship('TaskLog', backref='user')
 
 
 class TaskLog(Base):
